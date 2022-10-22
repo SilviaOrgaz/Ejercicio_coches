e751b94729554b1ef0f002120a4923f4c050cc12
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-22 18:27:41 +0200

    he cerado el string

--- ejercicio.py
+++ ejercicio.py
@@ -3,3 +3,6 @@ class Vehiculo:
     def __init__(self, col, n_ruedas):
         self.color = col
         self.ruedas = n_ruedas
+
+    def __str__(self):
+        return "El color del vehículo es {} y el número de ruedas es {}".format(self.color, self.ruedas)
