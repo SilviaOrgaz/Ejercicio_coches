083887fa94feca61144479b9dd6504844cc019b4
SilviaOrgaz <silvia.og.2309@gmail.com>
2022-10-25 18:16:27 +0200

    estoy probando un ejercicio

--- analisis_algoritmico/nodos.py/nodos_2.py
+++ analisis_algoritmico/nodos.py/nodos_2.py
@@ -26,3 +26,25 @@ class Polinomio(object):
                 actual = actual.sig
             aux.sig = actual.sig
             actual.sig = aux
+    def obtener_valor(polinomio, termino):
+        aux = polinomio.termino_mayor
+        while(aux is not None and aux.info.termino > termino):
+            aux = aux.sig
+        if(aux is not None and aux.info.termino == termino):
+            return aux.info.valor
+        else:
+            return 0
+
+    def restar(polinomio1, polinomio2):
+        aux = Polinomio()
+        menor = polinomio1 if (polinomio1.grado < polinomio2.grado) else polinomio2
+        for i in range(0, menor.grado+1):
+            total = Polinomio.obtener_valor(polinomio2, i) - Polinomio.obtener_valorobtener_valor(polinomio1, i)
+            if (total != 0):
+                Polinomio.agregar_termino(aux, i, total)
+
+A =  Polinomio()
+Polinomio1 = A.agregar_termino(2, 3)
+Polinomio2= A.agregar_termino(4, 5)
+restar_polinomio = A.restar(Polinomio2, Polinomio1)
+print(restar_polinomio)
